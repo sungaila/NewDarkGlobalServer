939b740c551b7c8e1c8fd973186ec7bf932c3666
David Sungaila <david.sungaila@arcor.de>
2026-03-03 20:45:43 +0100

    Update dockerfile

--- docker-build-push.bat
+++ docker-build-push.bat
@@ -1,2 +1,2 @@
 @echo off
-docker buildx build --no-cache --tag sungaila/newdarkglobalserver:1.4.0 --tag sungaila/newdarkglobalserver:latest --platform linux/amd64,linux/arm64 --push -f "src/GlobalServer/Dockerfile" .
+docker buildx build --pull --no-cache --tag sungaila/newdarkglobalserver:1.4.2 --tag sungaila/newdarkglobalserver:latest --platform linux/amd64,linux/arm64 --push -f "src/GlobalServer/Dockerfile" .
